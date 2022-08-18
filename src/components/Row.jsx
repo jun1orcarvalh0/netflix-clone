eb7375a7ba7411aa836120448a0580e59d0ae082
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-18 20:50:25 -0300

    terminando a parte 2 do netflix-clone

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,44 +1,63 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import { getMovies } from '../api';
-import './Row.css';
+import movieTrailer from "movie-trailer";
+import React, { useEffect } from "react";
+import ReactPlayer from "react-player";
+import { getMovies } from "../api";
+import "./Row.css";
 
 const imageHost = "https://image.tmdb.org/t/p/original/";
-function Row({title, path, isLarge}) {
+function Row({ title, path, isLarge }) {
   const [movies, setMovies] = React.useState([]);
-  
-  
+  const [trailerUrl, setTrailerUrl] = React.useState("");
+
+  const handleOnClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.name || movie.title || movie.original_name || "")
+        .then((url) => {
+          setTrailerUrl(url);
+        })
+        .catch((error) => {
+          console.log("Error fetching movie trailer: ", error);
+        });
+    }
+  };
+
   const fetchMovies = async (_path) => {
     try {
       const data = await getMovies(_path);
-      console.log('data :', data);
+      console.log("data :", data);
       setMovies(data?.results);
     } catch (error) {
-      console.log('fetchMovies error: ', error);
+      console.log("fetchMovies error: ", error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchMovies(path);
   }, [path]);
-  
 
   return (
-    <div className='row-container'>
-      <h2 className="row-header">{ title }</h2>
-      <div className='row-cards'>
+    <div className="row-container">
+      <h2 className="row-header">{title}</h2>
+      <div className="row-cards">
         {movies?.map((movie) => {
           return (
             <img
-              className={`movie-card ${isLarge && 'movie-card-large'}`}
+              onClick={() => handleOnClick(movie)}
+              className={`movie-card ${isLarge && "movie-card-large"}`}
               key={movie.id}
-              src={`${imageHost}${isLarge ? movie.backdrop_path : movie.poster_path}`} 
-              alt={movie.name} />
-          )
+              src={`${imageHost}${
+                isLarge ? movie.backdrop_path : movie.poster_path
+              }`}
+              alt={movie.name}
+            />
+          );
         })}
       </div>
+      {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
     </div>
   );
 }
 
-export default Row
+export default Row;
